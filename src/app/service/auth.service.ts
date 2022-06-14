f47b6860ddff90a39071420856902906d59674ca
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-14 23:58:57 +0200

    servicios

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const AUTH_API = "http://localhost:3000/api/auth/"
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpCliente: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.httpCliente.post(AUTH_API + 'signin', {
+      username: username,
+      password: password
+    }, httpOptions);
+  }
+
+  register(username: string, password: string, email: string): Observable<any> {
+    return this.httpCliente.post(AUTH_API + 'signup', {
+      username: username,
+      password: password,
+      email: email
+    }, httpOptions);
+  }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API_URL = "http://localhost:3000/api/test/"
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getPublicContent(): Observable<any> {
+    return this.httpClient.get(API_URL + 'all')
+  }
+  getUserBoard(): Observable<any> {
+    return this.httpClient.get(API_URL + 'user')
+  }
+  getModeratorBoard(): Observable<any> {
+    return this.httpClient.get(API_URL + 'mod')
+  }
+  getAdminBoard(): Observable<any> {
+    return this.httpClient.get(API_URL + 'admin')
+  }
 }
